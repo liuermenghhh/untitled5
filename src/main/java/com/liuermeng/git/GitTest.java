2c89ce9e444cc54002306a54bdadedf184dec098
liuermenghhh <1259492465@qq.com>
2021-07-02 14:39:24 +0800

    hot-fix first commit

--- src/main/java/com/liuermeng/git/GitTest.java
+++ src/main/java/com/liuermeng/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
         System.out.println("hello Git111");
+        System.out.println("hello Git2222222");
     }
 }
